34e0fa2956d64cfa096faf2e40068190682df043
Shaun <socgeology@gmail.com>
2020-12-06 21:14:09 -0400

    Add collisions, add and get mobs,tiles,items, refactor

--- import.spec.js
+++ import.spec.js
@@ -141,23 +141,154 @@ describe("Gameboard - Position and world", () => {
     }
   }
   })
-});
+  test("Add mob at x,y", ()=>{
+    let testMob = "a mob"
+    testGameboard.addMob(0,0,testMob)
+    expect(testGameboard.mobs[0]).toMatchObject({"x":0,"y":0,"mob":"a mob"})
+  })
+  test("Add tile at x,y", ()=>{
+    let testTile = "a tile"
+    testGameboard.addTile(0,0,testTile)
+    expect(testGameboard.tiles[0]).toMatchObject({"x":0,"y":0,"tile":"a tile"})
+  })
+  test("Add item at x,y", ()=>{
+    let testItem = "an item"
+    testGameboard.addItem(0,0,testItem)
+    expect(testGameboard.items[0]).toMatchObject({"x":0,"y":0,"item":"an item"})
+  })
+})
 
-describe("DungeonMaster - manage game for player", ()=>{
+describe("Collision - information about a proposed move", () => {
+  test("Make collision", ()=>{
+    let pcX = 1;
+    let pcY = 0;
+    let testCollision = new importMaps.Collision(pcX, pcY);
+    expect(testCollision.constructor.name).toEqual("Collision")
+  })
+  test("Return collision information", ()=>{
+    let proposedXMove = 1;
+    let proposedYMove = 0;
+    let mobs = []
+    let tiles = []
+    let items = []
+    let testCollision = new importMaps.Collision(proposedXMove,proposedYMove, mobs, tiles, items)
+    expect(testCollision.mobs).toEqual([])
+    expect(testCollision.tiles).toEqual([])
+    expect(testCollision.items).toEqual([])
+  })
+  test("Return collision information", ()=>{
+    let proposedXMove = 1;
+    let proposedYMove = 0;
+    let mobs = []
+    let tiles = [{"value":"road"}]
+    let items = []
+    let testCollision = new importMaps.Collision(proposedXMove,proposedYMove, mobs, tiles, items)
+    expect(testCollision.mobs).toEqual([])
+    expect(testCollision.tiles).toEqual([{"value":"road"}])
+    expect(testCollision.items).toEqual([])
+  })
+  test("Collect collision information and generate", ()=>{
+    let proposedXMove = 1;
+    let proposedYMove = 0;
+    let testGameboard = new importMaps.Gameboard;
+    let testMob = "a mob"
+    let testTile = "a tile"
+    let testItem = "an item"
+    let testX = testGameboard.pcXPosition + proposedXMove
+    let testY = testGameboard.pcYPosition + proposedYMove
+    //add testmob in tile to right of PC
+    testGameboard.addMob(testX,testY,testMob)
+    testGameboard.addTile(testX,testY,testTile)
+    testGameboard.addItem(testX,testY,testItem)
+    let mobs = testGameboard.getMobsAt(testX,testY)
+    let tiles = testGameboard.getTilesAt(testX,testY)
+    let items = testGameboard.getItemsAt(testX,testY)
+    let testCollision = new importMaps.Collision(proposedXMove,proposedYMove, mobs, tiles, items)
+    expect(testCollision.mobs).toMatchObject(["a mob"]);
+    expect(testCollision.tiles).toMatchObject(["a tile"]);
+    expect(testCollision.items).toMatchObject(["an item"]);
+  })
+})
+
+describe("DungeonMaster - manage game for player", () => {
   let dm = new importMaps.DungeonMaster
   test("Start new game", ()=>{
-    dm.mainLoop()
+    //dm.mainLoop()
   })
   test("Describe scene to player", ()=>{
-    let infoBundle = dm.gameBoard.describe();
+    let infoBundle = dm.gameboard.describe();
     //console.log(infoBundle)
     let description = dm.describe(infoBundle)
     expect(description).toEqual("You are standing on a road.");
   })
+  test("Given requested move, get collisions", ()=>{
+    let proposedXMove = 1;
+    let proposedYMove = 0;
+    let testMob = "a mob"
+    let testTile = "a tile"
+    let testItem = "an item"
+    let testX = dm.gameboard.pcXPosition + proposedXMove
+    let testY = dm.gameboard.pcYPosition + proposedYMove
+    //add testmob in tile to right of PC
+    dm.gameboard.addMob(testX,testY,testMob)
+    dm.gameboard.addTile(testX,testY,testTile)
+    dm.gameboard.addItem(testX,testY,testItem)
+    let result = dm.gameboard.getCollisions(proposedXMove,proposedYMove)
+    expect(result.mobs).toEqual(["a mob"])
+    expect(result.tiles).toEqual(["a tile"])
+    expect(result.items).toEqual(["an item"])
+  })
+  test("Given collisions, interpret for player",()=>{
+    let proposedXMove = 1;
+    let proposedYMove = 0;
+    let testMob = "a mob"
+    let testTile = "a tile"
+    let testItem = "an item"
+    let testX = dm.gameboard.pcXPosition + proposedXMove
+    let testY = dm.gameboard.pcYPosition + proposedYMove
+    //add testmob in tile to right of PC
+    dm.gameboard.addMob(testX,testY,testMob)
+    dm.gameboard.addTile(testX,testY,testTile)
+    dm.gameboard.addItem(testX,testY,testItem)
+    let testCollision = dm.gameboard.getCollisions(proposedXMove,proposedYMove)
+    let interpretation = dm.interpret(testCollision)
+    expect(interpretation).toEqual(`There is ${testCollision.mobs[0]}
+      and ${testCollision.items[0]} on the ${testCollision.tiles[0]}`)
+  })
+  test("Given proposed move, check if valid and do move", ()=>{
+    let proposedMove = "east";
+    dm.doMoveIfValid(proposedMove);
+    expect(dm.gameboard.pcXPosition).toEqual(191);
+    expect(dm.gameboard.pcYPosition).toEqual(130);
+
+  })
+  test("Given proposed invalid move, check if valid and do not move", ()=>{
+    let dm = new importMaps.DungeonMaster
+    let startX = dm.gameboard.pcXPosition;
+    let startY = dm.gameboard.pcYPosition;
+    //add building east of PC
+    dm.gameboard.addTile(startX + 1, startY, "building")
+    let proposedMove = "east";
+    //try to move into a wall
+    dm.doMoveIfValid(proposedMove);
+    expect(dm.gameboard.pcXPosition).toEqual(190);
+    expect(dm.gameboard.pcYPosition).toEqual(130);
+  })
+  
+  test("Given valid proposed move, make the move", ()=>{
+    //move is valid
+    let dm = new importMaps.DungeonMaster
+    let startX = 190;
+    let startY = 130;
+    dm.doMove("east")
+    expect(dm.gameboard.pcXPosition).toEqual(191);
+    expect(dm.gameboard.pcYPosition).toEqual(130);
+  })
+
   test("Look at the trees", ()=>{
     let result = ""
     for (i=0;i<27;i++){
-      result = dm.gameBoard.lookTo("east")
+      result = dm.gameboard.lookTo("east")
       //console.log(result)
     }
     expect(dm.describeLook(result)).toEqual("You see some trees.")
--- importMaps.js
+++ importMaps.js
@@ -187,6 +187,9 @@ class Gameboard{
     this.lookMode = false
     this.lookXPosition = this.pcXPosition;
     this.lookYPosition = this.pcYPosition;
+    this.mobs = []
+    this.tiles = []
+    this.items = []
   }
   //methods
   look(){
@@ -276,13 +279,73 @@ class Gameboard{
     let result = this.screen.getValueAtCoordinates(this.pcXPosition,this.pcYPosition)
     return result
   }
+  getObjectsAt(x,y,objectList, objectName){
+    //int x, int y, property objectList, str objectName
+    //return list of objects at x,y
+    let objects = objectList.filter(entry => entry["x"] == x && entry["y"] == y)
+    let results = objects.map(entry=>entry[objectName])
+    return results
+  }
+  getMobsAt(x,y){
+    //int x,y -> list of mobs at position
+    let result = this.getObjectsAt(x,y,this.mobs,"mob")
+    return result
+  }
+  getTilesAt(x,y){
+    //int x,y -> list of tiles at position
+    let result = this.getObjectsAt(x,y,this.tiles,"tile")
+    return result
+  }
+  getItemsAt(x,y){
+    //int x,y -> list of items at position
+    let result = this.getObjectsAt(x,y,this.items,"item")
+    return result
+  }
+  addMob(x,y,mob){
+    //place a mob at x,y
+    let newMobs = this.mobs.concat([{"x":x,"y":y,"mob":mob}])
+    this.mobs = newMobs
+  }
+  addTile(x,y,tile){
+    //place a tile at x,y
+    let newTiles = this.tiles.concat([{"x":x,"y":y,"tile":tile}])
+    this.tiles = newTiles
+  }
+  addItem(x,y,item){
+    //place a tile at x,y
+    let newItems = this.items.concat([{"x":x,"y":y,"item":item}])
+    this.items = newItems
+  }
+  getCollisions(proposedXMove,proposedYMove){
+    //int proposed move in x and y dirs
+    //return collision object
+    let testX = this.pcXPosition + proposedXMove
+    let testY = this.pcYPosition + proposedYMove
+    let mobs = this.getMobsAt(testX,testY)
+    let tiles = this.getTilesAt(testX,testY)
+    let items = this.getItemsAt(testX,testY)
+    let testCollision = new Collision(testX, testY, mobs, tiles, items)
+    return testCollision
+  }
+}
+
+class Collision{
+  constructor(pcX, pcY, mobs, tiles, items){
+    //pcX int, pcY int
+    this.proposedX = pcX;
+    this.proposedY = pcY;
+    //mobs, tiles, items = lists
+    this.mobs = mobs;
+    this.tiles = tiles;
+    this.items = items;
+  }
 }
 
 class DungeonMaster{
   //control game for player
   //keep track of turns, enemies, etc.
   constructor(){
-    this.gameBoard = new Gameboard
+    this.gameboard = new Gameboard
     this.descriptionMap = new Map([
       ["road","You are standing on a road."],
       ["tree","You are standing beneath a shady tree."],
@@ -300,7 +363,7 @@ class DungeonMaster{
   }
   announce(message){
     //write a message to the buffer
-    this.gameBoard.screen.writeMessage(message)
+    this.gameboard.screen.writeMessage(message)
   }
   describe(infoBundle){
     //interpret results for player
@@ -328,6 +391,46 @@ class DungeonMaster{
       return description
   }
 }
+  getOffsets(direction){
+    let offsetX = 0;
+    let offsetY = 0;
+    switch (direction){
+      case "north":
+        offsetY = -1
+        break;
+      case "east":
+        offsetX = 1
+        break;
+      case "south":
+        offsetY = 1
+        break;
+      case "west":
+        offsetX = -1
+        break;
+    }
+    return [offsetX,offsetY]
+  }
+  interpret(collision){
+    //collision Collision object
+    //do things and report to player
+    return `There is ${collision.mobs[0]}
+      and ${collision.items[0]} on the ${collision.tiles[0]}`
+  }
+  doMove(direction){
+    //str direction
+    //move the PC on the Gameboard
+    this.gameboard.movePC(direction)
+  }
+  doMoveIfValid(direction){
+    //str direction
+    //check if move is valid
+    let [xOffset, yOffset] = this.getOffsets(direction)
+    let collision = this.gameboard.getCollisions(xOffset, yOffset)
+    //check valid move tiles
+    if (collision.tiles[0] != "building"){
+      this.doMove(direction)
+    }
+  }
 
   mainLoop(){
     //draw map
@@ -336,14 +439,14 @@ class DungeonMaster{
     //pass information to Game
     //roll dice upon player action
       //this.screen.drawMap()
-    let result = this.gameBoard.describe()
-    this.gameBoard.screen.drawMap() //ewwww
+    let result = this.gameboard.describe()
+    this.gameboard.screen.drawMap() //ewwww
     this.describe(result)
     process.stdin.on('keypress', (key,data) =>{
       //default case doesn't pass a turn
       //look doesn't pass a turn
       //move passes a turn
-      //console.log(`x:${this.gameBoard.pcXPosition},y:${this.gameBoard.pcYPosition}`)
+      //console.log(`x:${this.gameboard.pcXPosition},y:${this.gameboard.pcYPosition}`)
       if (data.ctrl && data.name === 'q'){
         //quitmmport.spec.js
         process.exit()
@@ -353,25 +456,25 @@ class DungeonMaster{
           if (data.name ==  'x') {
             //exit look mode
             this.lookMode = false
-            this.gameBoard.screen.drawMap()
+            this.gameboard.screen.drawMap()
           } else {
             //look mode
             let lookResult;
             switch (data.name){
               case 'left':
-                lookResult = this.gameBoard.lookTo('west');
+                lookResult = this.gameboard.lookTo('west');
                 this.describeLook(lookResult)
                 break;
               case 'up':
-                lookResult = this.gameBoard.lookTo('north');
+                lookResult = this.gameboard.lookTo('north');
                 this.describeLook(lookResult)
                 break;
               case 'down':
-                lookResult = this.gameBoard.lookTo('south');
+                lookResult = this.gameboard.lookTo('south');
                 this.describeLook(lookResult)
                 break;
               case 'right':
-                lookResult = this.gameBoard.lookTo('east');
+                lookResult = this.gameboard.lookTo('east');
                 this.describeLook(lookResult)
                 break;
               }
@@ -380,39 +483,39 @@ class DungeonMaster{
           switch (data.name) {
             case 'left':
               //attempt move
-              if(this.gameBoard.attemptMove("west")){
+              if(this.gameboard.attemptMove("west")){
                 //do move
-                let result = this.gameBoard.movePC("west")
+                let result = this.gameboard.movePC("west")
                 //get info
                 this.describe(result)}
               break;
             case 'up':
               //attempt move
-              if(this.gameBoard.attemptMove("north")){
+              if(this.gameboard.attemptMove("north")){
                 //do move
-                let result = this.gameBoard.movePC("north")
+                let result = this.gameboard.movePC("north")
                 //get info
                 this.describe(result)}
               break;
             case 'down':
               //attempt move
-              if(this.gameBoard.attemptMove("south")){
+              if(this.gameboard.attemptMove("south")){
                 //do move
-                let result = this.gameBoard.movePC("south")
+                let result = this.gameboard.movePC("south")
                 //get info
                 this.describe(result)}
               break;
             case 'right':
               //attempt move
-              if(this.gameBoard.attemptMove("east")){
+              if(this.gameboard.attemptMove("east")){
                 //do move
-                let result = this.gameBoard.movePC("east")
+                let result = this.gameboard.movePC("east")
                 //get info
                 this.describe(result)}
               break;
             case 'l':
               this.lookMode = true
-              let result = this.gameBoard.describe()
+              let result = this.gameboard.describe()
               break;
             }//end switch
           }//end else
@@ -424,10 +527,11 @@ class DungeonMaster{
 exports.WorldMap = WorldMap;
 exports.Screen = Screen;
 exports.Gameboard = Gameboard;
+exports.Collision = Collision;
 exports.DungeonMaster = DungeonMaster;
 //decreasing y moves north in the map
 //increasing x moves east
 
-let dm = new DungeonMaster
-dm.gameBoard.screen.allowDrawing = true
-dm.mainLoop()
+//let dm = new DungeonMaster
+//dm.gameboard.screen.allowDrawing = true
+//dm.mainLoop()
